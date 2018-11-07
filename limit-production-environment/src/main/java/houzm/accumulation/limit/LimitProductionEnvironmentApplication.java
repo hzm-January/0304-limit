26cbbaaf8bbaa243435fb8042f32ed5e3dc8bafa
hzm-January <hzm_dream@163.com>
2018-11-07 17:44:32 +0800

    build limit production project

---
+++ limit-production-environment/src/main/java/houzm/accumulation/limit/LimitProductionEnvironmentApplication.java
@@ -0,0 +1,12 @@
+package houzm.accumulation.limit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitProductionEnvironmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitProductionEnvironmentApplication.class, args);
+	}
+}
